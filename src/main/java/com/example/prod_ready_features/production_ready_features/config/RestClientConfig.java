c7523069fafd34e15a6dcacbf040c07614027f23
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2024-07-30 11:40:42 +0530

    init

--- src/main/java/com/example/prod_ready_features/production_ready_features/config/RestClientConfig.java
+++ src/main/java/com/example/prod_ready_features/production_ready_features/config/RestClientConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
 
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
@@ -21,6 +22,10 @@ public class RestClientConfig {
         return RestClient.builder()
                 .baseUrl(BASE_URL)
                 .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .defaultStatusHandler(HttpStatusCode::is5xxServerError,((request, response) -> {
+
+                    throw new RuntimeException("Server Error Occurred");
+                }))
                 .build();
     }
 }
